992f6f8ed208aa4b5810877558331fadc7b5c557
Daniel <daniel.gutierrez@rakuten.com>
2024-02-07 15:40:50 +0100

    Some changes to web layout

--- dtv.py
+++ dtv.py
@@ -2,6 +2,14 @@ import streamlit as st
 from mitosheet.streamlit.v1 import spreadsheet
 
 #st.set_page_config(layout="wide")
+st.set_page_config(
+    page_title="DTV Demo",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'About': "# Example of a DTV Demo!"
+    }
+)
 
 CSV_URL = 'https://raw.githubusercontent.com/plotly/datasets/master/tesla-stock-price.csv'
 new_dfs, code = spreadsheet(CSV_URL)
