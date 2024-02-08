f3e4259af954cb47d2282d630878f3afb197ba0e
Daniel <daniel.gutierrez@rakuten.com>
2024-02-08 10:47:05 +0100

    Testing authentication

--- dtv.py
+++ dtv.py
@@ -18,14 +18,6 @@ name, authentication_status, username = authenticator.login('main')
 if st.session_state["authentication_status"]:
     authenticator.logout('Logout', 'main')
     st.write(f'Welcome *{name}*')
-    st.set_page_config(
-        page_title="DTV Demo",
-        layout="wide",
-        initial_sidebar_state="expanded",
-        menu_items={
-            'About': "# Example of a DTV Demo!"
-        }
-    )
 
     CSV_URL = 'https://raw.githubusercontent.com/plotly/datasets/master/tesla-stock-price.csv'
     new_dfs, _ = spreadsheet(CSV_URL)
