11e65f61083771370b859292744b42a672b58b38
Daniel <daniel.gutierrez@rakuten.com>
2024-02-08 12:11:09 +0100

    Added role filtering

--- dtv.py
+++ dtv.py
@@ -1,21 +1,22 @@
+import pandas as pd
 import streamlit as st
 import streamlit_authenticator as stauth
-from mitosheet.streamlit.v1 import spreadsheet
 import yaml
+from mitosheet.streamlit.v1 import spreadsheet
 
 st.set_page_config(
-        page_title="DTV Demo",
-        layout="wide",
-        initial_sidebar_state="expanded",
-        menu_items={
-            'About': "# Example of a DTV Demo!"
-        }
-    )
+    page_title="DTV Demo",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'About': "# Example of a DTV Demo!"
+    }
+)
 
 from yaml.loader import SafeLoader
+
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
-
 
 authenticator = stauth.Authenticate(
     config['credentials'],
@@ -29,10 +30,11 @@ if st.session_state["authentication_status"]:
     authenticator.logout('Logout', 'main')
     st.write(f'Welcome *{name}*')
 
-
     CSV_URL = 'dtv_data.csv'
-    new_dfs, _ = spreadsheet(CSV_URL)
-
+    df = pd.read_csv(CSV_URL)
+    if st.session_state["role"] != 'admin':
+        df = df[df["role"] == st.session_state["role"]]
+    new_dfs, _ = spreadsheet(df)
     st.write(new_dfs)
 elif st.session_state["authentication_status"] == False:
     st.error('Username/password is incorrect')
